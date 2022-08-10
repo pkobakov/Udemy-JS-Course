a0a4dcc11de78d1741f6e341c67c91dd86b417fd
Petar Kobakov <pkobakov@gmail.com>
2022-08-10 19:58:22 +0300

    Using Constants

--- lab/starting-project/assets/scripts/app.js
+++ lab/starting-project/assets/scripts/app.js
@@ -1,7 +1,8 @@
-let currentResult = 0;
+const defaultResult = 0;
+let currentResult = defaultResult;
 
 currentResult = (currentResult + 10) * 3;
 
-let calculationDescription = '('+ currentResult +' + 10) * 3';
+let calculationDescription = '('+ defaultResult +' + 10) * 3';
 
 outputResult(currentResult, calculationDescription);
