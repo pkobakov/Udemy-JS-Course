5e475a5b2ec72d30c4db58d471884d169174e909
Petar Kobakov <pkobakov@gmail.com>
2022-08-10 21:16:22 +0300

    Adding Custom function

--- lab/starting-project/assets/scripts/app.js
+++ lab/starting-project/assets/scripts/app.js
@@ -6,3 +6,12 @@ currentResult = (currentResult + 10) * 3;
 let calculationDescription = `( ${defaultResult} + 10) * 3`;
 let errorMessage = 'An error' + 'occured';
 outputResult(currentResult, calculationDescription);
+
+function add(num1, num2){
+  const result = num1+ num2;
+  alert(`The result is: ${result}`);
+}
+
+
+add(2,3);
+add(7,3);
