7c43287f772afef741ee1dfd9e11bcee462c007a
Petar Kobakov <pkobakov@gmail.com>
2022-08-10 19:54:19 +0300

    Numbers and Strings

--- lab/starting-project/assets/scripts/app.js
+++ lab/starting-project/assets/scripts/app.js
@@ -2,4 +2,6 @@ let currentResult = 0;
 
 currentResult = (currentResult + 10) * 3;
 
-outputResult(currentResult, '')
+let calculationDescription = '('+ currentResult +' + 10) * 3';
+
+outputResult(currentResult, calculationDescription);
