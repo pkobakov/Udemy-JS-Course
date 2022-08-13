325cf370df5a88fa447b78c47f5b7cbf70f6ed53
Petar Kobakov <pkobakov@gmail.com>
2022-08-13 16:07:22 +0300

    Adding More 'if' statements & 'Strong Attack' Functionality

--- exercise/control structures/assets/scripts/app.js
+++ exercise/control structures/assets/scripts/app.js
@@ -1,5 +1,6 @@
 const ATTACK_VALUE = 10;
 const MONSTER_ATTACK_VALUE = 14;
+const STRONG_ATTACK_VALUE = 17;
 
 let chosenMaxLife = 100;
 let currentMonsterHealth = chosenMaxLife;
@@ -7,8 +8,16 @@ let currentPlayerHealth = chosenMaxLife;
 
 adjustHealthBars(chosenMaxLife);
 
-function attackHandler() {
-    const damage = dealMonsterDamage(ATTACK_VALUE);
+function attackMonster(mode) {
+    let maxDamage;
+
+    if (mode === 'ATTACK') {
+        maxDamage = ATTACK_VALUE;
+    } else if (mode === 'STRONG_ATTACK') {
+        maxDamage = STRONG_ATTACK_VALUE;
+    }
+
+    const damage = dealMonsterDamage(maxDamage);
     currentMonsterHealth-=damage;
     const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
     currentPlayerHealth-=playerDamage;
@@ -21,7 +30,16 @@ function attackHandler() {
         alert('You have a draw!');
     }
 
+}
+
+function attackHandler() {
+    attackMonster('ATTACK');
 
 }
 
+function strongAttackHandler() {
+   attackMonster('STRONG_ATTACK');
+}
+
 attackBtn.addEventListener('click', attackHandler);
+strongAttackBtn.addEventListener('click', strongAttackHandler);
