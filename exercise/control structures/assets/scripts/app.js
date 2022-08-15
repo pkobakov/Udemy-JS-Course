93e947d67bc43ec0ba79f10dbe4f5efec922b024
Petar Kobakov <pkobakov@gmail.com>
2022-08-15 12:48:32 +0300

    Adding the Heal Functionality

--- exercise/control structures/assets/scripts/app.js
+++ exercise/control structures/assets/scripts/app.js
@@ -1,12 +1,26 @@
 const ATTACK_VALUE = 10;
 const MONSTER_ATTACK_VALUE = 14;
 const STRONG_ATTACK_VALUE = 17;
+const HEAL_VALUE = 20; 
 
 let chosenMaxLife = 100;
 let currentMonsterHealth = chosenMaxLife;
 let currentPlayerHealth = chosenMaxLife;
 
 adjustHealthBars(chosenMaxLife);
+
+function endRound() {
+    const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
+    currentPlayerHealth-=playerDamage;
+
+    if (currentMonsterHealth <= 0 && currentPlayerHealth > 0) {
+        alert('You won!');
+    } else if (currentPlayerHealth <= 0 && currentMonsterHealth > 0) {
+        alert('You lost!');
+    } else if (currentMonsterHealth <= 0 && currentPlayerHealth <= 0){ 
+        alert('You have a draw!');
+    }
+}
 
 function attackMonster(mode) {
     let maxDamage;
@@ -19,16 +33,7 @@ function attackMonster(mode) {
 
     const damage = dealMonsterDamage(maxDamage);
     currentMonsterHealth-=damage;
-    const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
-    currentPlayerHealth-=playerDamage;
-
-    if (currentMonsterHealth <= 0 && currentPlayerHealth > 0) {
-        alert('You won!');
-    } else if (currentPlayerHealth <= 0 && currentMonsterHealth > 0) {
-        alert('You lost!');
-    } else if (currentMonsterHealth <= 0 && currentPlayerHealth <= 0){ 
-        alert('You have a draw!');
-    }
+    endRound();
 
 }
 
@@ -41,5 +46,21 @@ function strongAttackHandler() {
    attackMonster('STRONG_ATTACK');
 }
 
+function healPlayerHandler() {
+ let healValue;
+ if (currentPlayerHealth >= chosenMaxLife-HEAL_VALUE) {
+
+    alert("You can't heal to more than max initial health.");
+    healValue = chosenMaxLife-currentPlayerHealth;
+ } else {
+    healValue = HEAL_VALUE;
+ }
+
+ increasePlayerHealth(healValue);
+ currentPlayerHealth+=healValue;
+ endRound();
+}
+
 attackBtn.addEventListener('click', attackHandler);
 strongAttackBtn.addEventListener('click', strongAttackHandler);
+healBtn.addEventListener('click', healPlayerHandler);
