eaae4a8d107b107a20137540bb0adaab8c2c11f2
Petar Kobakov <pkobakov@gmail.com>
2022-08-18 18:18:22 +0300

    Starting the Game. Creating the first function

---
+++ lab/more on functions/app.js
@@ -0,0 +1,8 @@
+const startGameBtn = document.getElementById('start-game-btn');
+
+
+function startGame() {
+    console.log('Game starting...');
+}
+
+startGameBtn?.addEventListener('click', startGame);
