95e5fb77a1572ae7d13ba6178867c0ca585bb9cd
Petar Kobakov <pkobakov@gmail.com>
2022-08-19 17:42:59 +0300

    Adding Function into other Function

--- exercise/more on functions/app.js
+++ exercise/more on functions/app.js
@@ -89,9 +89,15 @@ startGameBtn.addEventListener('click', () => {
 //not related to the Game:
 
 const sumUP = (...numbers) => {
+    const validateNumber = (number) => {
+        return isNaN(number)? 0 : number;
+    };
+
     let sum = 0;
+
     for (const num of numbers) {
-        sum+=num;
+        
+        sum+= validateNumber(num);
     }
 
     return sum;
