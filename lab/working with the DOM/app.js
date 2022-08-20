1e6a826a1ba056469544aad044b8bef6c7fa5116
Petar Kobakov <pkobakov@gmail.com>
2022-08-20 18:19:54 +0300

    Traversing the DOM and Styling DOM Elements

--- lab/working with the DOM/app.js
+++ lab/working with the DOM/app.js
@@ -1,13 +1,24 @@
 // const listItemElements = document.querySelectorAll('li');
 const listItemElements = document.getElementsByTagName('li');
+const button = document.querySelector('button');
+const section = document.querySelector('section');
 
 const h1 = document.getElementById('main-title');
 h1.textContent = 'Hello World';
 h1.style.color = 'white';
 h1.style.backgroundColor = 'teal';
 
+
+
+
 const li = document.querySelector('li:last-of-type');
 li.textContent = li.textContent + ' (Changed!)';
 for (const item of listItemElements) {
     console.log(item);
 }
+
+
+button.addEventListener('click', () => {
+    section.classList.toggle('visible');
+    section.classList.toggle('invisible');
+} )
