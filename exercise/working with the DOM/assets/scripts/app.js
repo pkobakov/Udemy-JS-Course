9f2430e935736528af2eca0d02fdca82b600952c
Petar Kobakov <pkobakov@gmail.com>
2022-08-22 13:21:39 +0300

    Fetching and Validating User Input

--- exercise/working with the DOM/assets/scripts/app.js
+++ exercise/working with the DOM/assets/scripts/app.js
@@ -2,8 +2,9 @@ const addMovieModal = document.getElementById('add-modal');
 const startAddMovieButton = document.querySelector('header button');
 const backdrop = document.getElementById('backdrop');
 // const backdrop = document.body.firstElementChild;
-
 const cancelAddMovieButton = addMovieModal.querySelector('.btn--passive');
+const confirmAddMovieButton = cancelAddMovieButton.nextElementSibling;
+const userInputs = addMovieModal.querySelectorAll('input');
 
 const toogleBackdrop = () => {
   backdrop.classList.toggle('visible');
@@ -14,16 +15,31 @@ const toggleMovieModal = () => {
   toogleBackdrop();
 };
 
-const cancelAddMovie = () => {
+const cancelAddMovieHandler = () => {
   toggleMovieModal();
 }; 
-
-
 
 const backdropClickHandler = () => {
  toggleMovieModal();
 };
 
+const addMovieHandler = () => {
+  const titleValue = userInputs[0].value;
+  const imageUrlValue = userInputs[1].value;
+  const ratingValue = userInputs[2].value;
+
+  if (titleValue.trim() === '' ||
+      imageUrlValue.trim() === '' ||
+      ratingValue.trim() === '' ||
+      +ratingValue < 1 ||
+      +ratingValue > 5) {
+    
+        alert('Please enter valid values (rating must be between 1 and 5).');
+        return; 
+  }
+};
+
 startAddMovieButton.addEventListener('click', toggleMovieModal);
-backdrop.addEventListener('click', toggleMovieModal);
-cancelAddMovieButton.addEventListener('click', cancelAddMovie);
+backdrop.addEventListener('click', backdropClickHandler);
+cancelAddMovieButton.addEventListener('click', cancelAddMovieHandler);
+confirmAddMovieButton.addEventListener('click', addMovieHandler);
