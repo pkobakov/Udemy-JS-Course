deae97000a824f04b931ca87e19285091fd24b6b
Petar Kobakov <pkobakov@gmail.com>
2022-08-24 21:17:23 +0300

    Reducing the Array of numbers

--- lab/more on arrays/assigment/app.js
+++ lab/more on arrays/assigment/app.js
@@ -4,7 +4,9 @@ const numbers = [1,2,3,4,5,6,7];
 // console.log(filteredNumers);
 
 
-const mappedNumbers = numbers.map( num => {number: num});
-console.log(mappedNumbers);
+// const mappedNumbers = numbers.map( num => ({number: num}));
+// console.log(mappedNumbers);
 
 
+const multiplication = numbers.reduce((currNum, nextNum) => currNum*nextNum, 1);
+console.log(multiplication);
