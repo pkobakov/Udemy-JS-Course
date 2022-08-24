8e8f638a49db71c4132b11e4650e37b1989e0b0b
Petar Kobakov <pkobakov@gmail.com>
2022-08-24 22:05:14 +0300

    Find the max value task.

--- lab/more on arrays/assigment/app.js
+++ lab/more on arrays/assigment/app.js
@@ -1,12 +1,32 @@
-const numbers = [1,2,3,4,5,6,7];
+const numbers = [1,2,100,3,4,5,6,7];
 
+//Part One.
+
+//Filter...
 // const filteredNumers = numbers.filter(number => number > 5); 
 // console.log(filteredNumers);
 
-
+//Mapping...
 // const mappedNumbers = numbers.map( num => ({number: num}));
 // console.log(mappedNumbers);
 
+//Reduce...
+// const multiplication = numbers.reduce((currNum, nextNum) => currNum*nextNum, 1);
+// console.log(multiplication);
 
-const multiplication = numbers.reduce((currNum, nextNum) => currNum*nextNum, 1);
-console.log(multiplication);
+
+//Part Two.
+
+function findMax(...nums) {
+  let currentMax = numbers[0];
+
+  for (const currentNum of nums) {
+    if (currentNum > currentMax) {
+        currentMax = currentNum;
+    }    
+  }
+
+  return currentMax;
+}
+
+console.log(findMax(...numbers));
