a823928336e56628dc557c2009d14c14b48a6027
Petar Kobakov <pkobakov@gmail.com>
2022-08-25 13:41:30 +0300

    Using property in square brackets

--- lab/objects/assets/scripts/objects.js
+++ lab/objects/assets/scripts/objects.js
@@ -1,6 +1,9 @@
+const anotherName = 'Alex';
+
 const person = {
     'first name': 'Petar',
     // name: 'Petar',
+    [anotherName]: 'Lilly',
     age: 47,
     hobbies: ['Sports', 'Cooking'],
     4.7 : 'hello',
@@ -25,4 +28,5 @@ const firstName = 'first name';
 
 console.log(`First name string property: ${person['first name']}`);
 console.log(`First name dynamic access: ${person[firstName]}`);
+console.log(person[anotherName]);
 console.log(person[4.7]);
