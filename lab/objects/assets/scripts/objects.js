e56b275141808663cc89edb21ab08e0d4c8e7ade
Petar Kobakov <pkobakov@gmail.com>
2022-08-25 15:13:08 +0300

    For-In loops. Outputting Dynamic Properties

--- lab/objects/assets/scripts/objects.js
+++ lab/objects/assets/scripts/objects.js
@@ -19,7 +19,14 @@ const renderMovies = () => {
 
     movies.forEach((movie) => {
         const movieElement = document.createElement('li');
-        movieElement.textContent = movie.info.title;
+        let text = movie.info.title + ' - ';
+
+        for(const key in movie.info){
+          if(key !== 'title') {
+            text = text + `${key}: ${movie.info[key]}`;
+          } 
+        }
+        movieElement.textContent = text;
         moviesList.append(movieElement);
     });
 
