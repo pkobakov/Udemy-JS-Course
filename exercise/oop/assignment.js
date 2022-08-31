52344ed430392d8f72de9f91e2bee457faab911a
Petar Kobakov <pkobakov@gmail.com>
2022-08-31 18:30:24 +0300

    Task Two.

--- exercise/oop/assignment.js
+++ exercise/oop/assignment.js
@@ -4,6 +4,14 @@ class Course {
         this.length = courseLenght;
         this.price = coursePrice;
     }
+
+    calculateValue() {
+        return this.length/this.price;
+    }
+
+    printSummary() {
+        console.log(`Title: ${this.title}, Length: ${this.length}, Price: ${this.price}`);
+    } 
 }
 
 const jsCourse = new Course ('JavaScript', 12, 11.99);
@@ -11,3 +19,9 @@ const cSharpCourse = new Course ('CSharp', 13, 11.99);
 
 console.log(jsCourse);
 console.log(cSharpCourse);
+
+console.log(jsCourse.calculateValue().toFixed(2));
+console.log(cSharpCourse.calculateValue().toFixed(2));
+
+console.log(jsCourse.printSummary());
+console.log(cSharpCourse.printSummary());
