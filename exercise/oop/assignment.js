d9c6ceb665e632a72652a58bf1bb00e87db926ac
Petar Kobakov <pkobakov@gmail.com>
2022-08-31 18:43:36 +0300

    Task Three.

--- exercise/oop/assignment.js
+++ exercise/oop/assignment.js
@@ -14,6 +14,8 @@ class Course {
     } 
 }
 
+
+
 const jsCourse = new Course ('JavaScript', 12, 11.99);
 const cSharpCourse = new Course ('CSharp', 13, 11.99);
 
@@ -25,3 +27,31 @@ console.log(cSharpCourse.calculateValue().toFixed(2));
 
 console.log(jsCourse.printSummary());
 console.log(cSharpCourse.printSummary());
+
+class PracticalCourse extends Course {
+    constructor(title, lenght, price, exercisesCount) {
+        super(title,lenght, price);
+
+        this.numOfExercises = exercisesCount;
+    }
+}
+
+const angularCourse = new PracticalCourse('Angular', 13, 11.99, 22);
+console.log(angularCourse);
+console.log(angularCourse.printSummary());
+
+class TheoreticalCourse extends Course {
+    constructor(title, length, price,) {
+        super(title, length, price );
+    }
+
+ publish() {
+    console.log('This is a nice course');
+ }   
+}
+
+
+const reactCourse = new TheoreticalCourse ('React', 15, 11.99);
+console.log(reactCourse);
+console.log(reactCourse.publish());
+
