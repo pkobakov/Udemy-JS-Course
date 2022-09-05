4efce42998814f4843010665e58166d17faedf8f
Petar Kobakov <pkobakov@gmail.com>
2022-09-06 00:12:18 +0300

    Callback Functions Examples

---
+++ lab/callBacks/app.js
@@ -0,0 +1,40 @@
+// EventListener Callback --------------
+
+// function greeting () {
+//     const input = document.querySelector('input').value;
+//     const output = document.querySelector('p');
+//     output.textContent = 'Hello ' +  input;
+
+// }
+
+// const buttonEl = document.querySelector('button');
+// buttonEl.addEventListener('click', greeting);
+
+// SetTimeout Callback ------------------
+
+// function firstAction (){
+//     console.log('Hello World');
+// }
+
+// setTimeout(firstAction, 5000);
+
+// setTimeout(function(){
+//     console.log('Hello from Petar')
+// }, 7000);
+
+// Custom Callback ------------
+
+// function greeting (input) {
+//     console.log('Hello from ' + input);
+// }
+
+// function userInput (callback) {
+//     const name = 'Petar';
+
+//     callback(name);
+// } 
+
+
+// setTimeout(() => {
+//     userInput(greeting)
+// }, 5000);
