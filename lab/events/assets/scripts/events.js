385fe6d9b3010b7c151f96d9d7c6a55ebecdb1a0
Petar Kobakov <pkobakov@gmail.com>
2022-09-09 01:50:44 +0300

    Delegation Pattern

--- lab/events/assets/scripts/events.js
+++ lab/events/assets/scripts/events.js
@@ -32,25 +32,44 @@ form.addEventListener('submit', event => {
 
 //Propagation
 
-const clickMeButton = document.querySelector('button');
-clickMeButton.addEventListener('click', (event) => {
-    // event.stopPropagation();
-    console.log('BUTTON CLICKED');
-    console.log(event);
+// const clickMeButton = document.querySelector('button');
+// clickMeButton.addEventListener('click', (event) => {
+//     // event.stopPropagation();
+//     console.log('BUTTON CLICKED');
+//     console.log(event);
+// });
+
+// const clickMeTooButton = document.querySelector('button:last-of-type');
+// clickMeTooButton.addEventListener('click', event => {
+//     // event.stopImmediatePropagation();
+//     console.log('ME TOO BUTTON CLICKED');
+//     console.log(event);
+// })
+
+// const div = document.querySelector('div');
+// div.addEventListener('click', (event) => {
+//     console.log('DIV CLICKED');
+//     console.log(event);
+// });
+
+
+// const listItems = document.querySelectorAll('li');
+
+// listItems.forEach(listItem => {
+//     listItem.addEventListener('click', event => {
+//         event.target.classList.toggle('highlight');
+//     });
+// });
+
+// Event Delegation Pattern:
+
+const list = document.querySelector('ul');
+list.addEventListener('click', event => {
+    event.target.closest('li').classList.toggle('highlight');
 });
 
-const clickMeTooButton = document.querySelector('button:last-of-type');
-clickMeTooButton.addEventListener('click', event => {
-    // event.stopImmediatePropagation();
-    console.log('ME TOO BUTTON CLICKED');
-    console.log(event);
-})
 
-const div = document.querySelector('div');
-div.addEventListener('click', (event) => {
-    console.log('DIV CLICKED');
-    console.log(event);
-}); 
+
 
 
 
