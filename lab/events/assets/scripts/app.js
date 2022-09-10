13230adab2f29ab523b8b93969b4b752d059593e
Petar Kobakov <pkobakov@gmail.com>
2022-09-11 00:34:18 +0300

    Dropping and Moving Data and Elements

--- lab/events/assets/scripts/app.js
+++ lab/events/assets/scripts/app.js
@@ -106,9 +106,14 @@ class ProjectItem {
   }
 
   connectDrag() {
-    document.getElementById(this.id).addEventListener('dragstart', event => {
+    const item = document.getElementById(this.id);
+    item.addEventListener('dragstart', event => {
       event.dataTransfer.setData('text/plain', this.id);
       event.dataTransfer.effectAllowed = 'move';
+    })
+
+    item.addEventListener('dragend',  event => {
+      console.log(event);
     })
 
   }
@@ -177,7 +182,23 @@ connectDroppable() {
       list.parentElement.classList.remove('droppable');
     }
      
+
   });
+
+  list.addEventListener('drop', event => {
+    const projId = event.dataTransfer.getData('text/plain');
+    if (this.projects.find(p => p.id === projId)) {
+      return;
+    }
+
+    document
+    .getElementById(projId)
+    .querySelector('button:last-of-type')
+    .click();
+    list.parentElement.classList.remove('droppable');
+    // event.preventDefault();       
+
+  })
 }
 
   setSwitchHandlerFunction(switchHandlerFunction) {
