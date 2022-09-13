7a1b63362a99b0ee8d67a7c3ba6a62a6a0e61fad
Petar Kobakov <pkobakov@gmail.com>
2022-09-13 18:01:20 +0300

    Tagged Templates

--- lab/nums and strings advanced/app.js
+++ lab/nums and strings advanced/app.js
@@ -1,5 +1,34 @@
-function randomInBetween (min, max) {
-   return Math.floor(Math.random()*max)+min;
+// function randomIntBetween (min, max) {
+//    return Math.floor(Math.random()*(max-min+1)+min);
+// }
+
+// console.log(randomIntBetween(5, 10));
+
+
+function productDescription(strings, name, price) {
+    return `${strings[0]}${name}${strings[1]}${price}`;
 }
 
-console.log(randomInBetween(5, 10));
+const nameInput = 'JavaScript Complete Guide';
+const priceInput = 29.99;
+
+const courseDescription = productDescription`This course ${nameInput} costs ${priceInput}`;
+
+console.log(courseDescription);
+
+function calculatedPrice(strings, name, price) {
+    
+   return `${strings[0]}${name}${strings[1]}${price}${strings[2]}`;
+};
+
+const course = 'JavaScript';
+const price = 11.99;
+const result = calculatedPrice`The ${course} course costs: ${price}$`;
+
+console.log(result);
+
+
+
+
+
+
