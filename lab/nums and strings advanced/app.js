f93c2d412f238c2b687815b07c24ed02e1018698
Petar Kobakov <pkobakov@gmail.com>
2022-09-13 18:38:08 +0300

    Regular Expressions

--- lab/nums and strings advanced/app.js
+++ lab/nums and strings advanced/app.js
@@ -1,36 +1,43 @@
+//Random numbers....
+
 // function randomIntBetween (min, max) {
 //    return Math.floor(Math.random()*(max-min+1)+min);
 // }
 
 // console.log(randomIntBetween(5, 10));
 
+//Tagged Templates....
 
-function productDescription(strings, name, price) {
-    const tax = 1.2;
-    const totalPrice = (price*tax).toFixed(2);
-    return `${strings[0]}${name}${strings[1]}${totalPrice}${strings[2]}`;
-}
+// function productDescription(strings, name, price) {
+//     const tax = 1.2;
+//     const totalPrice = (price*tax).toFixed(2);
+//     return `${strings[0]}${name}${strings[1]}${totalPrice}${strings[2]}`;
+// }
 
-const nameInput = 'JavaScript Complete Guide';
-const priceInput = 29.99;
+// const nameInput = 'JavaScript Complete Guide';
+// const priceInput = 29.99;
 
-const courseDescription = productDescription`This course ${nameInput} costs ${priceInput}$`;
+// const courseDescription = productDescription`This course ${nameInput} costs ${priceInput}$`;
 
-console.log(courseDescription);
+// console.log(courseDescription);
 
-function calculatedPrice(strings, name, price) {
+// function calculatedPrice(strings, name, price) {
     
-   return `${strings[0]}${name}${strings[1]}${price}${strings[2]}`;
-};
+//    return `${strings[0]}${name}${strings[1]}${price}${strings[2]}`;
+// };
 
-const course = 'JavaScript';
-const price = 11.99;
-const result = calculatedPrice`The ${course} course costs: ${price}$`;
+// const course = 'JavaScript';
+// const price = 11.99;
+// const result = calculatedPrice`The ${course} course costs: ${price}$`;
 
-console.log(result);
+// console.log(result);
 
+//Regex....
 
+const template = /^\S+@\S+\./;
+const email = 'test@test.com';
 
+console.log(template.test(email));
 
 
 
