0c75aa4faa458b26e74496fb28cfeb97ae0a5b21
Petar Kobakov <pkobakov@gmail.com>
2022-09-14 16:13:51 +0300

    Blocking Code and The Event Loop

--- lab/async/app.js
+++ lab/async/app.js
@@ -6,3 +6,13 @@ function trackUserHandler() {
 }
 
 button.addEventListener('click', trackUserHandler);
+
+let result = 0;
+
+
+for (let index = 0; index < 1000000000; index++) {
+  result+=index;
+  
+}
+
+console.log(result);
