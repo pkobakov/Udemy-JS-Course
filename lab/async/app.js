d1b04dafa75aa2b47a25bb4546d7e35a61933fe5
Petar Kobakov <pkobakov@gmail.com>
2022-09-14 17:02:24 +0300

    Multiple Callbacks and setTimeout(0)

--- lab/async/app.js
+++ lab/async/app.js
@@ -2,17 +2,32 @@ const button = document.querySelector('button');
 const output = document.querySelector('p');
 
 function trackUserHandler() {
-  console.log('Clicked!');
+  navigator.geolocation.getCurrentPosition(
+    postData => {
+      setTimeout(() => {
+      console.log(postData);
+      }, 2000);
+    },
+    error => {
+      console.log(error);
+    }
+  );
+
+  setTimeout(() => {
+    console.log('Timer done!');
+  }, 0);
+
+  console.log('Getting location...');
 }
 
 button.addEventListener('click', trackUserHandler);
 
-let result = 0;
+// let result = 0;
 
 
-for (let index = 0; index < 1000000000; index++) {
-  result+=index;
+// for (let index = 0; index < 1000000000; index++) {
+//   result+=index;
   
-}
+// }
 
-console.log(result);
+// console.log(result);
