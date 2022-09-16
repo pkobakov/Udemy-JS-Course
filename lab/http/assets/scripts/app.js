2fc60ffce442eebf02647229d315f23bd4dbb751
Petar Kobakov <pkobakov@gmail.com>
2022-09-16 17:40:31 +0300

    Starting with Http Requests, transforming the response data into JS code and displaying the output in the Browser

--- lab/http/assets/scripts/app.js
+++ lab/http/assets/scripts/app.js
@@ -1,1 +1,24 @@
 // https://jsonplaceholder.typicode.com/
+
+const listOfElements = document.querySelector('.posts');
+const postTemplate = document.getElementById('single-post');
+
+const xhr = new XMLHttpRequest();
+xhr.open('GET', 'https://jsonplaceholder.typicode.com/posts');
+
+// xhr.responseType = 'json'; - another way to configure the response data from json to js
+
+xhr.onload = function () {
+    const listOfPosts = JSON.parse(xhr.response);
+    console.log(listOfPosts);
+
+    for (const post of listOfPosts) {
+        const postEl = document.importNode(postTemplate.content, true);
+        postEl.querySelector('h2').textContent = post.title.toUpperCase();
+        postEl.querySelector('p').textContent = post.body;
+        listOfElements.append(postEl);
+    }
+
+}
+
+xhr.send();
