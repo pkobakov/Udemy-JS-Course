984fa999fd21ea373f7a6bcbaab33ab3b0f687e8
Petar Kobakov <pkobakov@gmail.com>
2022-09-21 14:00:00 +0300

    Setting Up the entry.

---
+++ lab/events/src/app.js
@@ -0,0 +1,32 @@
+import { ProjectList } from './App/ProjectList.js';
+
+// window.DEFAULT_VALUE = 'This is the window DEFAULT VALUE';
+globalThis.DEFAULT_VALUE = 'This is the globalThis DEFAULT VALUE';
+
+export class App {
+  static init() {
+    const activeProjectsList = new ProjectList('active');
+    const finishedProjectsList = new ProjectList('finished');
+    activeProjectsList.setSwitchHandlerFunction(
+      finishedProjectsList.addProject.bind(finishedProjectsList)
+    );
+    finishedProjectsList.setSwitchHandlerFunction(
+      activeProjectsList.addProject.bind(activeProjectsList)
+    );
+
+    // const timerId = setTimeout(this.startAnalytics, 3000);
+
+    // document.getElementById('stop-analytics-btn').addEventListener('click', () => {
+    //   clearTimeout(timerId);
+    // });
+  }
+
+  static startAnalytics() {
+    const analyticsScript = document.createElement('script');
+    analyticsScript.src = 'assets/scripts/Utility/Analytics.js';
+    analyticsScript.defer = true;
+    document.head.append(analyticsScript);
+  }
+}
+
+App.init();
---
+++ lab/events/src/events.js
@@ -0,0 +1,89 @@
+const button = document.querySelector('button');
+button.addEventListener('click', event => {
+ console.log('BUTTON WAS CLICKED');
+ console.log(event);
+});
+
+// const buttons = document.querySelectorAll('button');
+
+// const buttonClickHandler = event => {
+//     // event.target.disabled = true;
+//     console.log(event);
+// }
+
+// const anotherButtonClickHandler = () => {
+//     console.log('Button was clicked');
+// }
+// // button.onclick = buttonClickHandler();
+
+// const boundFn = buttonClickHandler.bind(this);
+
+
+// button.addEventListener('click', buttonClickHandler);
+
+// setTimeout(() => {button.removeEventListener('click', buttonClickHandler); 
+// }, 2000);
+
+// buttons.forEach(btn => {
+//     btn.addEventListener('click', buttonClickHandler);
+// });
+
+// window.addEventListener('scroll', event => {console.log(event)});
+
+const form = document.querySelector('form');
+form.addEventListener('submit', function(event) {
+    event.preventDefault();
+    console.log(event);
+    console.log(this);
+
+});
+
+//Propagation
+
+// const clickMeButton = document.querySelector('button');
+// clickMeButton.addEventListener('click', (event) => {
+//     // event.stopPropagation();
+//     console.log('BUTTON CLICKED');
+//     console.log(event);
+// });
+
+// const clickMeTooButton = document.querySelector('button:last-of-type');
+// clickMeTooButton.addEventListener('click', event => {
+//     // event.stopImmediatePropagation();
+//     console.log('ME TOO BUTTON CLICKED');
+//     console.log(event);
+// })
+
+// const div = document.querySelector('div');
+// div.addEventListener('click', (event) => {
+//     console.log('DIV CLICKED');
+//     console.log(event);
+// });
+
+
+// const listItems = document.querySelectorAll('li');
+
+// listItems.forEach(listItem => {
+//     listItem.addEventListener('click', event => {
+//         event.target.classList.toggle('highlight');
+//     });
+// });
+
+// Event Delegation Pattern:
+
+const list = document.querySelector('ul');
+list.addEventListener('click', function(event) {
+    event.target.closest('li').classList.toggle('highlight');
+    console.log(this);
+    button.click();
+});
+
+
+
+
+
+
+
+
+
+
