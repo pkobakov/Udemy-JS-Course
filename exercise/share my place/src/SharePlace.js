6243b920445bbe4e3d922e717a0aab77b707297e
Petar Kobakov <pkobakov@gmail.com>
2022-09-29 16:38:33 +0300

    Getting the User Location

--- exercise/share my place/src/SharePlace.js
+++ exercise/share my place/src/SharePlace.js
@@ -3,12 +3,29 @@ class PlaceFinder {
     const addressForm = document.querySelector('form');
     const locateUserBtn = document.getElementById('locate-btn');
 
-    locateUserBtn.addEventListener('click', locateUserHandler);
-    addressForm.addEventListener('click', findAddressHandler);
+    locateUserBtn.addEventListener('click', this.locateUserHandler);
+    addressForm.addEventListener('click', this.findAddressHandler);
 
    }
 
    locateUserHandler(){
+    if (!navigator.geolocation) {
+        alert('Location feature is not available. Please use more modern browser or enter the address manually.');
+        return;
+    }
+
+    navigator.geolocation.getCurrentPosition(
+        sucessResult => {
+            console.log(sucessResult);
+            const coordinates = {
+                lat: sucessResult.coords.latitude + Math.random() * 50,
+                lng: sucessResult.coords.longitude + Math.random() * 50
+            };
+            console.log(coordinates);
+        }, 
+        error => {
+       alert('Could not locate you. Please enter the location manually.')
+    })
 
    }
 
@@ -16,3 +33,5 @@ class PlaceFinder {
 
    } 
 }
+
+const placeFinder = new PlaceFinder();
