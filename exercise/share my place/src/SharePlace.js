7ee8201c420acbff2440b178598393582a67df7f
Petar Kobakov <pkobakov@gmail.com>
2022-09-29 15:13:50 +0300

    Get DOM Access

---
+++ exercise/share my place/src/SharePlace.js
@@ -0,0 +1,18 @@
+class PlaceFinder {
+   constructor(){
+    const addressForm = document.querySelector('form');
+    const locateUserBtn = document.getElementById('locate-btn');
+
+    locateUserBtn.addEventListener('click', locateUserHandler);
+    addressForm.addEventListener('click', findAddressHandler);
+
+   }
+
+   locateUserHandler(){
+
+   }
+
+   findAddressHandler(){
+
+   } 
+}
