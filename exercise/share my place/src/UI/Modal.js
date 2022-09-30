acf8f8b2b365d3549c8c79177b7918b13118a330
Petar Kobakov <pkobakov@gmail.com>
2022-09-30 11:02:43 +0300

    Hiding the Modal

--- exercise/share my place/src/UI/Modal.js
+++ exercise/share my place/src/UI/Modal.js
@@ -9,14 +9,14 @@ export class Modal {
     show() {
       if('content' in document.createElement('template')) {
         const modalElements = document.importNode(this.modalTemplateEl.content, true);
-        const modalElement = modalElements.querySelector('.modal');
-        const backdropElement = modalElements.querySelector('.backdrop');
+        this.modalElement = modalElements.querySelector('.modal');
+        this.backdropElement = modalElements.querySelector('.backdrop');
         const contentElement = document.importNode(this.contentTemplateEl.content, true);
 
-        modalElement.appendChild(contentElement);
+        this.modalElement.appendChild(contentElement);
 
-        document.body.insertAdjacentElement('afterbegin', modalElement);
-        document.body.insertAdjacentElement('afterbegin', backdropElement);
+        document.body.insertAdjacentElement('afterbegin', this.modalElement);
+        document.body.insertAdjacentElement('afterbegin', this.backdropElement);
       }
        else {
         alert();
@@ -24,6 +24,12 @@ export class Modal {
     }
 
     hide() {
+      if (this.modalElement) {
+        document.body.removeChild(this.modalElement);
+        document.body.removeChild(this.backdropElement);
+        this.modalElement = null;
+        this.backdropElement = null;
+      }
 
     }
 }
