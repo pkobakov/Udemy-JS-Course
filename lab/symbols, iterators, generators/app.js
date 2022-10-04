f8aa43894af99006d264c2e9059180cda0d4962f
Petar Kobakov <pkobakov@gmail.com>
2022-10-04 18:06:31 +0300

    Well-Known Symbols

--- lab/symbols, iterators, generators/app.js
+++ lab/symbols, iterators, generators/app.js
@@ -5,12 +5,14 @@ const person = {
     id: 'p1',
     [uid]: '1', 
     name: 'Peter',
-    age: 47
+    age: 47,
+    [Symbol.toStringTag]: 'User'
 };
 
 person.id = 'p2';
 console.log(person[Symbol('uid')]);
 console.log(Symbol('uid') === Symbol('uid'));
+console.log(person.toString());
 
 
 
