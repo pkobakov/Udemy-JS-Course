7adec81481a22f6ce3abece75308a5d58eaead14
Petar Kobakov <pkobakov@gmail.com>
2022-10-05 15:59:37 +0300

    Understanding Iterators

--- lab/symbols, iterators, generators/app.js
+++ lab/symbols, iterators, generators/app.js
@@ -15,4 +15,25 @@ console.log(Symbol('uid') === Symbol('uid'));
 console.log(person.toString());
 
 
+const company = {
+    currEmployee: 0,
+    employees: ['Anna','Max','Peter'],
+    next(){
+        if (this.currEmployee >= this.employees.length) {
+            return {value: this.currEmployee, done: true};
+        }
+        const returnValue = {
+            value: this.employees[this.currEmployee],
+            done: false
+        };
 
+        this.currEmployee++;
+        return returnValue; 
+    }
+};
+
+
+console.log(company.next());
+console.log(company.next());
+console.log(company.next());
+console.log(company.next());
