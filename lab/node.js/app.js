b3ba26a4a119115777f77f7ea8c94c7e935672a9
Petar Kobakov <pkobakov@gmail.com>
2022-10-06 14:03:56 +0300

    Working with Incoming request

--- lab/node.js/app.js
+++ lab/node.js/app.js
@@ -1,24 +1,8 @@
-// const userName = 'Petar';
-// console.log(`Hi ${userName}`);
-// console.log('Hello World!');
+const http = require('http');
 
-
-const fs = require('fs');
-
-
-
-fs.writeFile('user-data.txt', 'username= Peter', err => {
-    if (err) {
-        console.log(err);
-    } else {
-        console.log('Wrote to file');
-    }
+const server = http.createServer((request, response) => {
+    response.write('hello there!');
+    response.end();
 });
 
-fs.readFile('user-data.txt', (err, data) => {
-    if (err) {
-        console.log(err);
-        return;
-    }
-    console.log(data.toString());
-});
+server.listen(3000);
