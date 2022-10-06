d0008410707a185c7c12534a6beffcd39cbb7685
Petar Kobakov <pkobakov@gmail.com>
2022-10-06 13:53:58 +0300

    Modules and File Access

--- lab/node.js/app.js
+++ lab/node.js/app.js
@@ -1,3 +1,24 @@
-const userName = 'Petar';
-console.log(`Hi ${userName}`);
-console.log('Hello World!');
+// const userName = 'Petar';
+// console.log(`Hi ${userName}`);
+// console.log('Hello World!');
+
+
+const fs = require('fs');
+
+
+
+fs.writeFile('user-data.txt', 'username= Peter', err => {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log('Wrote to file');
+    }
+});
+
+fs.readFile('user-data.txt', (err, data) => {
+    if (err) {
+        console.log(err);
+        return;
+    }
+    console.log(data.toString());
+});
