8e67252726d54577fd535312350f8a8727e83650
Petar Kobakov <pkobakov@gmail.com>
2022-10-08 23:31:56 +0300

    Understanding the CORS

--- lab/node.js/app.js
+++ lab/node.js/app.js
@@ -10,6 +10,13 @@ const app = express();
 
 app.use(bodyParser.json());
 
+app.use(( req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'POST, GET, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+})
+
 app.use(locationRoutes);
 
 // app.use((req, res, next) => {
