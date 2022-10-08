e92c66a2462c87b96661b98b7a4a0a597c3d511b
Petar Kobakov <pkobakov@gmail.com>
2022-10-09 00:20:17 +0300

    Adding the Get Location Route

--- exercise/share my place/src/MyPlace.js
+++ exercise/share my place/src/MyPlace.js
@@ -10,10 +10,24 @@ class LoadedPlace {
 
 const url = new URL(location.href);
 const queryParams = url.searchParams;
-const coords = {
-lat: parseFloat(queryParams.get('lat')),
-lng: +queryParams.get('lng')
-};
+// const coords = {
+// lat: parseFloat(queryParams.get('lat')),
+// lng: +queryParams.get('lng')
+// };
 
-const address = queryParams.get('address'); 
-new LoadedPlace(coords,address);
+// const address = queryParams.get('address');
+const locId = queryParams.get('location');
+fetch('http://localhost:3000/location/' + locId)
+.then(response => {
+    if (response.status ===  404) {
+        throw new Error('Could not find location!')
+    }
+    return response.json();
+})
+.then( data => {
+    new LoadedPlace(data.coordinates, data.address);
+
+})
+.catch(error => {
+    alert(error.message);
+}); 
