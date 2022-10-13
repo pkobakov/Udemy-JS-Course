817d605c7d7d7a1f12854c2ce1ea092feb1761c0
Petar Kobakov <pkobakov@gmail.com>
2022-10-13 16:19:04 +0300

    Updating the DOM Correctly

---
+++ lab/performance/src/optimized/product-management.js
@@ -0,0 +1,43 @@
+import { renderProducts, updateProducts } from './rendering';
+import { products as prods} from './products';
+
+let products = prods;
+
+
+
+export function deleteProduct(prodId) {
+  const updatedProducts = [];
+  let deletedProd;
+  for (const prod of products) {
+    if (prod.id !== prodId) {
+      updatedProducts.push(prod);
+    } else {
+      deletedProd = prod;
+    }
+  }
+  products = updatedProducts;
+  updateProducts(deletedProd, prodId, deleteProduct, false);
+}
+
+export function addProduct(event) {
+  event.preventDefault();
+  const titleEl = document.querySelector('#new-product #title');
+  const priceEl = document.querySelector('#new-product #price');
+
+  const title = titleEl.value;
+  const price = priceEl.value;
+
+  if (title.trim().length === 0 || price.trim().length === 0 || +price < 0) {
+    alert('Please enter some valid input values for title and price.');
+    return;
+  }
+
+  const newProduct = {
+    id: new Date().toString(),
+    title: title,
+    price: price
+  };
+
+  products.unshift(newProduct);
+  renderProducts(products, deleteProduct);
+}
---
+++ lab/performance/src/optimized/rendering.js
@@ -0,0 +1,38 @@
+export function renderProducts(products, deleteProductFn) {
+  const productListEl = document.getElementById('product-list');
+  productListEl.innerHTML = '';
+  products.forEach(product => {
+    const newListEl = document.createElement('li');
+    const prodTitleEl = document.createElement('h2');
+    const prodPriceEl = document.createElement('p');
+    const prodDeleteButtonEl = document.createElement('button');
+
+    prodTitleEl.innerHTML = product.title;
+    prodPriceEl.innerHTML = product.price;
+    prodDeleteButtonEl.innerHTML = 'DELETE';
+
+    newListEl.id = product.id;
+
+    prodDeleteButtonEl.addEventListener(
+      'click',
+      deleteProductFn.bind(null, product.id)
+    );
+
+    newListEl.appendChild(prodTitleEl);
+    newListEl.appendChild(prodPriceEl);
+    newListEl.appendChild(prodDeleteButtonEl);
+
+    productListEl.appendChild(newListEl);
+  });
+}
+
+export function updateProducts( product, prodId, deleteProductfn, isAdding ) {
+  if (isAdding) {
+    
+    
+  } else {
+    const prodEl = document.getElementById(prodId);
+    prodEl.remove();
+
+  }
+}
