28c0b81d9c75c32b9742a4ca35a6ab1aba9d09f9
Petar Kobakov <pkobakov@gmail.com>
2022-10-18 18:20:33 +0300

    Working with Types

---
+++ lab/typescript intro/app.js
@@ -0,0 +1,5 @@
+function writeText(text1, text2) {
+    return text1 + text2;
+}
+var result = writeText('Hello ', 'World!');
+console.log(result);
---
+++ lab/typescript intro/app.ts
@@ -0,0 +1,6 @@
+function writeText (text1:string, text2:string) : string {
+  return text1 + text2;
+}
+
+const result = writeText('Hello ', 'World!');
+console.log(result);
