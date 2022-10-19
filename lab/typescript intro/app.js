bcdc65d9b11f5ddcb3cf0c04e545932b46b1da1a
Petar Kobakov <pkobakov@gmail.com>
2022-10-19 16:59:26 +0300

    Core Types Dive Deeper

--- lab/typescript intro/app.js
+++ lab/typescript intro/app.js
@@ -1,5 +1,19 @@
-function writeText(text1, text2) {
-    return text1 + text2;
+var input1 = document.getElementById('number-1');
+var input2 = document.getElementById('number-2');
+var buttonElement = document.querySelector('button');
+function add(a, b) {
+    return a + b;
 }
-var result = writeText('Hello ', 'World!');
-console.log(result);
+function printResult(result) {
+    console.log(result);
+}
+// const result = writeText('Hello ', 'World!');
+// let isDone = true;
+// printResult(result);
+buttonElement === null || buttonElement === void 0 ? void 0 : buttonElement.addEventListener('click', function () {
+    console.log('The sum is:');
+    var num1 = +input1.value;
+    var num2 = +input2.value;
+    var result = add(num1, num2);
+    printResult(result);
+});
--- lab/typescript intro/app.ts
+++ lab/typescript intro/app.ts
@@ -1,6 +1,23 @@
-function writeText (text1:string, text2:string) : string {
-  return text1 + text2;
+const input1 = document.getElementById('number-1') as HTMLInputElement;
+const input2 = document.getElementById('number-2') as HTMLInputElement;
+const buttonElement = document.querySelector('button');
+
+function add(a:number, b:number) {
+  return a + b;
 }
 
-const result = writeText('Hello ', 'World!');
-console.log(result);
+function printResult(result) {
+    console.log(result);
+}
+
+// const result = writeText('Hello ', 'World!');
+// let isDone = true;
+// printResult(result);
+
+buttonElement?.addEventListener('click', () => {
+    console.log('The sum is:');
+    const num1 = +input1.value;
+    const num2 = +input2.value;
+    const result = add(num1, num2);
+    printResult(result);
+});
