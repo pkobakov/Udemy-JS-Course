196cf50b0f5326a67fcdcf9bfcd6a66415efc5a3
Petar Kobakov <pkobakov@gmail.com>
2022-10-20 18:38:25 +0300

    Using Attributes

--- lab/web components/tooltip.js
+++ lab/web components/tooltip.js
@@ -2,11 +2,16 @@ class Tooltip extends HTMLElement {
     constructor () {
         super();
         this._tooltipContainer;
-
+        this._tooltipText = 'Some dummy tooltip text';
+        
     }
-   
+    
     connectedCallback() {
-
+        if (this.hasAttribute('text')) {
+            this._tooltipText = this.getAttribute('text');
+            
+        }
+        
         const tooltipIcon = document.createElement('span');
         tooltipIcon.textContent = ' :))';
         tooltipIcon.addEventListener('mouseenter', this._showTooltip.bind(this));
@@ -17,7 +22,7 @@ class Tooltip extends HTMLElement {
     _showTooltip() {
 
         this._tooltipContainer = document.createElement('div');
-        this._tooltipContainer.textContent = 'This is the tooltip text!';
+        this._tooltipContainer.textContent = this._tooltipText;
         this.appendChild(this._tooltipContainer);
 
     }
