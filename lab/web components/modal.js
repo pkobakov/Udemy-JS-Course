0b79a1f7f1333e55c36901ae8d9c62870d1b0fab
Petar Kobakov <pkobakov@gmail.com>
2022-10-26 16:00:46 +0300

    Adding the Modal Container

--- lab/web components/modal.js
+++ lab/web components/modal.js
@@ -12,6 +12,19 @@ class ModalComponent extends HTMLElement {
             width: 100%;
             height: 100vh;
             background: rgba(0,0,0,0.75);
+            z-index: 10;
+        }
+
+        #modal {
+            position: fixed;
+            top: 15vh;
+            left: 25%;
+            width: 50%;
+            height: 30rem;
+            background: white;
+            border-radius: 3px;
+            z-index: 100;
+            box-shadow: 0 2px 8px rgba(0,0,0, 0.26);
         }
         </style>
         <div id="backdrop"></div>
