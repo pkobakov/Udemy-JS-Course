5ba0ec07c02d05b0a1a3f48e87d5c088f28bcaaf
Petar Kobakov <pkobakov@gmail.com>
2022-10-26 17:15:32 +0300

    Styling the Modal Elements

--- lab/web components/modal.js
+++ lab/web components/modal.js
@@ -5,7 +5,7 @@ class ModalComponent extends HTMLElement {
         this.attachShadow({mode: 'open'});
         this.shadowRoot.innerHTML = `
         <style>
-        #backdrop {
+         #backdrop {
             position: fixed;
             top: 0;
             left: 0;
@@ -13,22 +13,63 @@ class ModalComponent extends HTMLElement {
             height: 100vh;
             background: rgba(0,0,0,0.75);
             z-index: 10;
-        }
+         }
 
-        #modal {
+         #modal {
             position: fixed;
             top: 15vh;
             left: 25%;
             width: 50%;
-            height: 30rem;
             background: white;
             border-radius: 3px;
             z-index: 100;
             box-shadow: 0 2px 8px rgba(0,0,0, 0.26);
-        }
+            display: flex;
+            flex-direction: column;
+            justify-content: space-between;
+
+          }
+
+          header {
+            padding: 1rem;
+          }
+
+          header h1 {
+            font-size: 1.25rem;
+          }
+
+          #main {
+            padding: 1rem;
+          }
+
+          #actions {
+            border-top: 1px solid #ccc;
+            padding: 1rem;
+            display: flex;
+            justify-content: flex-end;
+          }
+          
+          #actions button {
+            margin: 0 0.25rem;
+          }
+
+
         </style>
         <div id="backdrop"></div>
-        <div id="modal"></div>
+        <div id="modal">
+           <header>
+              <h1>Please Confirm</h1>
+           </header>
+           <section id="main">
+              <slot></slot>
+           </section >
+
+           <section id="actions">
+             <button>Cancel</button>
+             <button>OK</button>
+           </section>
+
+        </div>
         `;
     }
 }
