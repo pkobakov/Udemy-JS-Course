efd2c580ef96626e0a409e7c6f1050ca54795511
Petar Kobakov <pkobakov@gmail.com>
2022-10-28 14:17:55 +0300

    Finishing Up

--- lab/web components/modal.js
+++ lab/web components/modal.js
@@ -1,150 +1,142 @@
-class ModalComponent extends HTMLElement {
-
-    constructor() {
-        super();
-        this.isOpen = false;
-        this.attachShadow({mode: 'open'});
-        this.shadowRoot.innerHTML = `
+class MyModal extends HTMLElement {
+  constructor() {
+    super();
+    this.attachShadow({ mode: 'open' });
+    this.isOpen = false;
+    this.shadowRoot.innerHTML = `
         <style>
-         #backdrop {
-            position: fixed;
-            top: 0;
-            left: 0;
-            width: 100%;
-            height: 100vh;
-            background: rgba(0,0,0,0.75);
-            z-index: 10;
-            opacity: 0;
-            pointer-events: none;
-         }
+            #backdrop {
+                position: fixed;
+                top: 0;
+                left: 0;
+                width: 100%;
+                height: 100vh;
+                background: rgba(0,0,0,0.75);
+                z-index: 10;
+                opacity: 0;
+                pointer-events: none;
+            }
 
-         #modal {
-            position: fixed;
-            top: 15vh;
-            left: 25%;
-            width: 50%;
-            background: white;
-            border-radius: 3px;
-            z-index: 100;
-            box-shadow: 0 2px 8px rgba(0,0,0, 0.26);
-            display: flex;
-            flex-direction: column;
-            justify-content: space-between;
-            opacity: 0;
+            :host([opened]) #backdrop,
+            :host([opened]) #modal {
+                opacity: 1;
+                pointer-events: all;
+            }
 
-          }
+            :host([opened]) #modal {
+                top: 15vh;
+            }
 
-          :host([open]) #backdrop,
-          :host([open]) #modal {
-            opacity: 1;
-            poiter-events: all;
-          }
+            #modal {
+                position: fixed;
+                top: 10vh;
+                left: 25%;
+                width: 50%;
+                z-index: 100;
+                background: white;
+                border-radius: 3px;
+                box-shadow: 0 2px 8px rgba(0,0,0,0.26);
+                display: flex;
+                flex-direction: column;
+                justify-content: space-between;
+                opacity: 0;
+                pointer-events: none;
+                transition: all 0.3s ease-out;
+            }
 
-          header {
-            padding: 1rem;
-          }
+            header {
+                padding: 1rem;
+                border-bottom: 1px solid #ccc;
+            }
 
-          ::slotted(h1) {
-            font-size: 1.25rem;
-          }
+            ::slotted(h1) {
+                font-size: 1.25rem;
+                margin: 0;
+            }
 
-          #main {
-            padding: 1rem;
-          }
+            #main {
+                padding: 1rem;
+            }
 
-          #actions {
-            border-top: 1px solid #ccc;
-            padding: 1rem;
-            display: flex;
-            justify-content: flex-end;
-          }
-          
-          #actions button {
-            margin: 0 0.25rem;
-          }
+            #actions {
+                border-top: 1px solid #ccc;
+                padding: 1rem;
+                display: flex;
+                justify-content: flex-end;
+            }
 
-
+            #actions button {
+                margin: 0 0.25rem;
+            }
         </style>
         <div id="backdrop"></div>
         <div id="modal">
-           <header>
-              <slot name="title"></slot>
-           </header>
-           <section id="main">
-              <slot></slot>
-           </section >
-
-           <section id="actions">
-             <button id="confirm-btn">OK</button>
-             <button id="cancel-btn">Cancel</button>
-           </section>
+            <header>
+                <slot name="title">Please Confirm Payment</slot>
+            </header>
+            <section id="main">
+                <slot></slot>
+            </section>
+            <section id="actions">
+                <button id="cancel-btn">Cancel</button>
+                <button id="confirm-btn">Okay</button>
+            </section>
         </div>
-        `;
+    `;
+    const slots = this.shadowRoot.querySelectorAll('slot');
+    slots[1].addEventListener('slotchange', event => {
+      console.dir(slots[1].assignedNodes());
+    });
+    const backdrop = this.shadowRoot.querySelector('#backdrop');
+    const cancelButton = this.shadowRoot.querySelector('#cancel-btn');
+    const confirmButton = this.shadowRoot.querySelector('#confirm-btn');
+    backdrop.addEventListener('click', this._cancel.bind(this));
+    cancelButton.addEventListener('click', this._cancel.bind(this));
+    confirmButton.addEventListener('click', this._confirm.bind(this));
+    // cancelButton.addEventListener('cancel', () => {
+    //   console.log('Cancel inside the component');
+    // });
+  }
 
-        const slots = this.shadowRoot.querySelectorAll('slot');
-        const slot = slots[1];
-        slot.addEventListener('slotchange', () => {
-          console.dir(slot.assignedNodes());
-
-        });
-
-        const confirmBtn = this.shadowRoot.getElementById('confirm-btn');
-        confirmBtn.addEventListener('click', this._confirm.bind(this));
-        const cancelBtn = this.shadowRoot.getElementById('cancel-btn');
-        cancelBtn.addEventListener('click', this._cancel.bind(this));
-        cancelBtn.addEventListener('cancel', () => {
-          console.log('Cancel inside the component.');
-        })
-        
-
-
-
-
-
-    }
-
-    // attributeChangedCallback(name, oldValue, newValue) {
-    //   if (name === 'open') {
-    //     if (this.hasAttribute('open')) {
-    //       this.shadowRoot.querySelector('#backdrop').style.opacity = 1;
-    //       this.shadowRoot.querySelector('#backdrop').style.pointerEvents = 'all';
-    //       this.shadowRoot.querySelector('#modal').style.opacity = 1;
-    //       this.shadowRoot.querySelector('#modal').style.pointerEvents = 'all';
-    //     }
-    //   }
-    // }
-
-    // static get observedAttributes() {
-    //   return ['open'];
-    // }
-
-    open() {
-      this.setAttribute('open', '');
-      this.isOpen  = true;
-    };
-
-    hide() {
-      if (this.hasAttribute('open')) {
-        
-        this.removeAttribute('open');
-      }
-
+  attributeChangedCallback(name, oldValue, newValue) {
+    if (this.hasAttribute('opened')) {
+      this.isOpen = true;
+      // this.shadowRoot.querySelector('#backdrop').style.opacity = 1;
+      // this.shadowRoot.querySelector('#backdrop').style.pointerEvents = 'all';
+      // this.shadowRoot.querySelector('#modal').style.opacity = 1;
+      // this.shadowRoot.querySelector('#modal').style.pointerEvents = 'all';
+    } else {
       this.isOpen = false;
     }
+  }
 
-    _cancel(event) {
-       this.hide();
-       const cancelEvent = new Event('cancel', {bubbles: true, composed: true});
-       event.target.dispatchEvent(cancelEvent);
+  static get observedAttributes() {
+    return ['opened'];
+  }
 
-    };
+  open() {
+    this.setAttribute('opened', '');
+    this.isOpen = true;
+  }
 
-    _confirm() {
-       this.hide();
-       const confirmEvent = new Event ('confirm');
-       this.dispatchEvent(confirmEvent);
+  hide() {
+    if (this.hasAttribute('opened')) {
+      this.removeAttribute('opened');
     }
+    this.isOpen = false;
+  }
 
+  _cancel(event) {
+    this.hide();
+    const cancelEvent = new Event('cancel', { bubbles: true, composed: true });
+    event.target.dispatchEvent(cancelEvent);
+  }
+
+  _confirm() {
+    this.hide();
+    const confirmEvent = new Event('confirm');
+    this.dispatchEvent(confirmEvent);
+  }
 }
 
-customElements.define('my-modal', ModalComponent);
+customElements.define('my-modal', MyModal);
